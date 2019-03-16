7e1d6e86aa01941db0f4ad59b2f2219ebd87afad
Maciej Pięta <maciej.pieta16@gmail.com>
2019-03-16 14:47:57 +0100

    My first application in React

---
+++ precache-manifest.c95ba342b017dd5a0cb7a0acf53a056f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a93925dc9f074d07d18",
+    "url": "/static/js/main.0413808b.chunk.js"
+  },
+  {
+    "revision": "032baba43fed69e6c73d",
+    "url": "/static/js/2.51bc3250.chunk.js"
+  },
+  {
+    "revision": "4a93925dc9f074d07d18",
+    "url": "/static/css/main.6622238f.chunk.css"
+  },
+  {
+    "revision": "032baba43fed69e6c73d",
+    "url": "/static/css/2.7411c59d.chunk.css"
+  },
+  {
+    "revision": "11260fa2e0f5cdd31ea37b44b133f13e",
+    "url": "/index.html"
+  }
+];
